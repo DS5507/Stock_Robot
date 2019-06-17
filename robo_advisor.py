487676bfa09a24e2679fef4f88fc6d504acf1def
DS5507 <50781786+DS5507@users.noreply.github.com>
2019-06-17 14:19:00 -0400

    Issue a request

--- robo_advisor.py
+++ robo_advisor.py
@@ -1,13 +1,16 @@
 ## robo_advisor.py
 
 import requests
+import json
 
 
 
-
-request_url = "XX"
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo"
 
 response = request.get(request_url)
-print(type(response))
-print(response.status_code)
-print(response.text)
+#print(type(response)) #><class 'requests.models.Response'>
+#print(response.status_code) #> 200
+#print(response.text) #>
+ 
+  
+parsed_response = json.loads(response.text) #> type=dict
