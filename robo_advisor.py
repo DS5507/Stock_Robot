7f2a479cc86faa53e0f6e3097025b7c95fd75066
DS5507 <50781786+DS5507@users.noreply.github.com>
2019-06-17 14:27:57 -0400

    Get latest refresh date

--- robo_advisor.py
+++ robo_advisor.py
@@ -14,3 +14,29 @@ response = request.get(request_url)
  
   
 parsed_response = json.loads(response.text) #> type=dict
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+
+
+
+#
+# Info Outputs
+#
+
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print(f"LATEST DAY: "{last_refreshed})
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
