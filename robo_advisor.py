4b26eedb72b20b75bd6e96ca9922ed7af8d4fe37
DS5507 <50781786+DS5507@users.noreply.github.com>
2019-06-18 19:53:11 -0400

    Writing example data to CSV

--- robo_advisor.py
+++ robo_advisor.py
@@ -2,6 +2,7 @@
 
 import csv
 import json
+import os
 
 import requests
 
@@ -49,6 +50,17 @@ recent_low = min(low_prices) # TODO: Confirm low price
 # Info Outputs
 #
 
+csv_file_path = os.path.join(os.path.dirname(__file__), "data", "prices.csv")
+
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
@@ -63,17 +75,8 @@ print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
-print("WRITING DATA TO CSV...")
+print(f"WRITING DATA TO CSV: {csv_file_path}")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
 
-csv_file_path = "data/prices.csv" # a relative filepath
-
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
-    writer.writeheader() # uses fieldnames set above
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
